abd546ea4c13d2ebbf8f8ef9f64e7c50be75e028
MunhozSilva <luciano.asimov@gmail.com>
2022-01-19 21:51:23 -0300

    findViewById set on configuration button

--- app/src/main/java/com/example/chessclock/MainActivity.kt
+++ app/src/main/java/com/example/chessclock/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.chessclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import android.widget.Toast.makeText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val configButton = findViewById<ImageButton>(R.id.configurationButton)
+
+        configButton.setOnClickListener { makeText(this, "Ok", Toast.LENGTH_SHORT).show() }
     }
 }
