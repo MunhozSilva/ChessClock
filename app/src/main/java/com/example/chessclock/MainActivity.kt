04854485fe99d221a7ddd360d6c0c51061d74a45
MunhozSilva <luciano.asimov@gmail.com>
2022-02-15 09:03:49 -0300

    Binding setup

--- app/src/main/java/com/example/chessclock/MainActivity.kt
+++ app/src/main/java/com/example/chessclock/MainActivity.kt
@@ -6,16 +6,21 @@ import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.Toast
 import android.widget.Toast.makeText
+import com.example.chessclock.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val configButton = findViewById<ImageButton>(R.id.configurationButton)
         val toastMessage: String = resources.getString(R.string.toast_text)
 
-        configButton.setOnClickListener {
+        binding.configurationButton.setOnClickListener {
             makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
             val intent = Intent(this, ConfigurationActivity::class.java)
             startActivity(intent)
